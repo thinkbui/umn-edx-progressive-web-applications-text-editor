ea2cf27d24df193d890e5b68de9a976dc40ad057
Ian Bui <thinkbui@gmail.com>
2023-05-26 15:56:31 -0700

    Set client db config

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,15 +25,15 @@ export const putDb = async (content) => {
   // First, create a variable, and set it to asyncronously await the opening of the database. Replace the items in all caps
   
   // TODO: Change YOUR_OPEN_DB_VAR to whatever variable name you wanT. Note that you'll then need to change any other occcurences of YOUR_OPEN_DB_VAR to the same variable name.
-  const YOUR_OPEN_DB_VAR = await openDB(DB_NAME, 1);
+  const openedDb = await openDB(DB_NAME, 1);
 
   // TODO: Now create a variable for the transaction; again, this will be referenced below.
-  const YOUR_TX_VAR = YOUR_OPEN_DB_VAR.transaction(DB_NAME, 'readwrite');
+  const dbTxn = openedDb.transaction(DB_NAME, 'readwrite');
 
   // TODO: Now create a variable for the store
-  const YOUR_STORE_VAR = YOUR_TX_VAR.objectStore(DB_NAME);
+  const dbStore = dbTxn.objectStore(DB_NAME);
 
-  const request = YOUR_STORE_VAR.put({ id: 1, value: content });
+  const request = dbStore.put({ id: 1, value: content });
   const result = await request;
   console.log('🚀 - data saved to the database', result.value);
 };
@@ -45,6 +45,9 @@ export const getDb = async () => {
   // You can duplicate the same lines of code from above, except that the transaction will be 'readonly'
   
   // TODO: Copy LINES 28, 31 and 34 above; the new line 31 code should be "readonly"
+  const openedDb = await openDB(DB_NAME, 1);
+  const dbTxn = openedDb.transaction(DB_NAME, 'readonly');
+  const dbStore = dbTxn.objectStore(DB_NAME);
 
   // Leave the rest as-is
   const request = store.get(1);

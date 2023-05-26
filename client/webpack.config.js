8f81c7187f46cb9481739ca96a1d9a1176b20ad6
Ian Bui <thinkbui@gmail.com>
2023-05-26 15:17:44 -0700

    Added WebpackPwaManifest config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -73,7 +73,24 @@ module.exports = () => {
         swSrc: './src-sw.js',
         swDest: 'src-sw.js'
       }),
-      new WebpackPwaManifest({})
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E',
+        description: "This is an example text editor app for learning PWA.",
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ]
+      })
 
     ],
 
